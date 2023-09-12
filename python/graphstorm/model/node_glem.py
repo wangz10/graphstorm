253b78fd3f0e8aecc81a050861e3d28c70bd597f
Zichen Wang <zichewan@amazon.com>
2023-09-12 01:29:16 +0000

    Bug fix GLEM `target_ntype`

--- python/graphstorm/model/node_glem.py
+++ python/graphstorm/model/node_glem.py
@@ -42,6 +42,7 @@ class GLEM(GSgnnNodeModelBase):
     """
     def __init__(self,
                  alpha_l2norm,
+                 target_ntype,
                  em_order_gnn_first=False,
                  inference_using_gnn=True,
                  pl_weight=0.5,
@@ -49,6 +50,7 @@ class GLEM(GSgnnNodeModelBase):
                  ):
         super(GLEM, self).__init__()
         self.alpha_l2norm = alpha_l2norm
+        self.target_ntype = target_ntype
         self.em_order_gnn_first = em_order_gnn_first
         self.inference_using_gnn = inference_using_gnn
         self.pl_weight = pl_weight
@@ -354,17 +356,18 @@ class GLEM(GSgnnNodeModelBase):
         input_nodes : {target_ntype: tensor.shape [bs], other_ntype: []}
         blocks : list[dgl.Block]
         """
-        target_ntype = blocks[-1].dsttypes[0]
+        # target_ntype = blocks[-1].dsttypes[0]
+        target_ntype = self.target_ntype
         n_seed_nodes = blocks[-1].num_dst_nodes()
         return {target_ntype: input_nodes[target_ntype][:n_seed_nodes]}
 
-    def _embed_nodes(self, blocks, node_feats, _, input_nodes=None, do_gnn_encode=True):
+    def _embed_nodes(self, blocks, node_feats, _, input_nodes=None, do_gnn_encode=True, target_ntype=None):
         """ Embed and encode nodes with LM, optionally followed by GNN encoder for GLEM model
         """
         if do_gnn_encode:
             # Get the projected LM embeddings without GNN message passing
             encode_embs = self.lm.comput_input_embed(input_nodes, node_feats)
-            target_ntype = list(encode_embs.keys())[0]
+            target_ntype = self.target_ntype
             # GNN message passing
             encode_embs_gnn = self.gnn.gnn_encoder(blocks, encode_embs)
             n_seed_nodes = blocks[-1].num_dst_nodes()
@@ -373,7 +376,7 @@ class GLEM(GSgnnNodeModelBase):
             # Get the projected LM embeddings for seed nodes:
             seed_nodes = self._get_seed_nodes(input_nodes, blocks)
             encode_embs = self.lm.comput_input_embed(seed_nodes, node_feats)
-            target_ntype = list(encode_embs.keys())[0]
+            target_ntype = self.target_ntype
             return encode_embs[target_ntype], None
 
     def _process_labels(self, labels):
